9ffb2df07a46c23e58c2bc97ebe183f2567c0388
Tuomas Suutari <tuomas@nepnep.net>
2024-01-29 15:16:23 +0200

    Lisää Metat

--- kysely/models.py
+++ kysely/models.py
@@ -5,6 +5,10 @@ class Kysymys(models.Model):
     teksti = models.CharField(max_length=200)
     julkaisupvm = models.DateTimeField("julkaistu")
 
+    class Meta:
+        verbose_name = "kysymys"
+        verbose_name_plural = "kysymykset"
+
     def __str__(self):
         return self.teksti
 
@@ -13,6 +17,10 @@ class Vaihtoehto(models.Model):
     kysymys = models.ForeignKey(Kysymys, on_delete=models.CASCADE)
     teksti = models.CharField(max_length=200)
     ääniä = models.IntegerField(default=0)
+
+    class Meta:
+        verbose_name = "vaihtoehto"
+        verbose_name_plural = "vaihtoehdot"
 
     def __str__(self):
         return self.teksti
