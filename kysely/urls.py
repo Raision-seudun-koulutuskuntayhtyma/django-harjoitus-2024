074c15c686ec356af3cf44f5da387689374d108b
Tuomas Suutari <tuomas@nepnep.net>
2024-01-29 15:36:56 +0200

    Lisää muutama kysely-näkymä lisää

--- kysely/urls.py
+++ kysely/urls.py
@@ -3,5 +3,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.indeksi, name="index"),
+    # esim. /kyselyt/
+    path("", views.indeksi, name="indeksi"),
+    # esim. /kyselyt/5/
+    path("<int:question_id>/", views.näytä, name="näytä"),
+    # esim. /kyselyt/5/results/
+    path("<int:question_id>/tulokset/", views.tulokset, name="tulokset"),
+    # esim. /kyselyt/5/äänestä/
+    path("<int:question_id>/äänestä/", views.äänestä, name="äänestä"),
 ]
--- kysely/views.py
+++ kysely/views.py
@@ -4,3 +4,15 @@ from django.http import HttpResponse
 
 def indeksi(request):
     return HttpResponse("Heippa! Olet kysely-appin index-sivulla.")
+
+
+def näytä(request, question_id):
+    return HttpResponse(f"Katsot juuri kysymystä {question_id}")
+
+
+def tulokset(request, question_id):
+    return HttpResponse(f"Katsot kysymyksen {question_id} tuloksia")
+
+
+def äänestä(request, question_id):
+    return HttpResponse(f"Olet äänestämässä kysymykseen {question_id}")
