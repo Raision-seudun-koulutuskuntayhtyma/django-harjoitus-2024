032aabb0d2c4b95129cc1bf6c7f6582dc2eaaf88
Tuomas Suutari <tuomas@nepnep.net>
2024-01-29 15:10:47 +0200

    admin-parannus ja str-metodit

--- kysely/admin.py
+++ kysely/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
 
-from .models import Kysymys
+from .models import Kysymys, Vaihtoehto
 
 admin.site.register(Kysymys)
+
+@admin.register(Vaihtoehto)
+class VaihtoehtoAdmin(admin.ModelAdmin):
+    list_display = ["kysymys", "teksti"]
--- kysely/models.py
+++ kysely/models.py
@@ -5,8 +5,14 @@ class Kysymys(models.Model):
     teksti = models.CharField(max_length=200)
     julkaisupvm = models.DateTimeField("julkaistu")
 
+    def __str__(self):
+        return self.teksti
+
 
 class Vaihtoehto(models.Model):
     kysymys = models.ForeignKey(Kysymys, on_delete=models.CASCADE)
     teksti = models.CharField(max_length=200)
     ääniä = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.teksti
