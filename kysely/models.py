2ef3b4806c1e3513e9c6e8988ac5cb8ead0f17d7
Tuomas Suutari <tuomas@nepnep.net>
2024-01-29 15:00:53 +0200

    Kysely-appin models

--- kysely/models.py
+++ kysely/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Kysymys(models.Model):
+    teksti = models.CharField(max_length=200)
+    julkaisupvm = models.DateTimeField("julkaistu")
+
+
+class Vaihtoehto(models.Model):
+    kysymys = models.ForeignKey(Kysymys, on_delete=models.CASCADE)
+    teksti = models.CharField(max_length=200)
+    ääniä = models.IntegerField(default=0)
