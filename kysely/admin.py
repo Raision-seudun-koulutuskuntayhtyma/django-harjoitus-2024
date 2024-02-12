5498ae935ec87a3c2128cfb5034610b80533eb47
Tuomas Suutari <tuomas@nepnep.net>
2024-02-12 15:55:01 +0200

    Lisää TabularInline KysymysAdminiin

--- kysely/admin.py
+++ kysely/admin.py
@@ -2,7 +2,20 @@ from django.contrib import admin
 
 from .models import Kysymys, Vaihtoehto
 
-admin.site.register(Kysymys)
+
+class VastausvaihtoehtoInline(admin.TabularInline):
+    model = Vaihtoehto
+    extra = 3
+
+
+@admin.register(Kysymys)
+class KysymysAdmin(admin.ModelAdmin):
+    fieldsets = [
+        ("Päivämäärätiedot", {"fields": ["julkaisupvm"]}),
+        ("Sisältö", {"fields": ["teksti"]}),
+    ]
+    inlines = [VastausvaihtoehtoInline]
+
 
 @admin.register(Vaihtoehto)
 class VaihtoehtoAdmin(admin.ModelAdmin):
