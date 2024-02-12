cf7334b2fa1bcab1b9955f68f02a318da33eddb7
Tuomas Suutari <tuomas@nepnep.net>
2024-02-12 11:55:04 +0200

    Korjaa onko_julkaistu_lähiaikoina ja lisää testit

--- kysely/models.py
+++ kysely/models.py
@@ -16,7 +16,8 @@ class Kysymys(models.Model):
         return self.teksti
 
     def onko_julkaistu_lähiaikoina(self):
-        return self.julkaisupvm >= timezone.now() - datetime.timedelta(days=1)
+        nyt = timezone.now()
+        return nyt - datetime.timedelta(days=1) <= self.julkaisupvm <= nyt
 
 
 class Vaihtoehto(models.Model):
--- kysely/tests.py
+++ kysely/tests.py
@@ -1,3 +1,43 @@
-from django.test import TestCase
+import datetime
 
-# Create your tests here.
+from django.test import TestCase
+from django.utils import timezone
+
+from .models import Kysymys
+
+
+class KysymysModelTests(TestCase):
+    def test_onko_julkaistu_lähiaikoina_tulevaisuuden_kysymyksellä(self):
+        """
+        onko_julkaistu_lähiaikoina on False jos kysymys tulevaisuudessa.
+        """
+        tulevaisuuden_aika = timezone.now() + datetime.timedelta(days=30)
+        tuleva_kysymys = Kysymys(julkaisupvm=tulevaisuuden_aika)
+
+        vastaus = tuleva_kysymys.onko_julkaistu_lähiaikoina()
+
+        self.assertIs(vastaus, False)
+
+    def test_onko_julkaistu_lähiaikoina_vanhalla_kysymyksellä(self):
+        """
+        onko_julkaistu_lähiaikoina False jos vanhempi kuin 1 pv
+        """
+        päivä_ja_yksi_sek = datetime.timedelta(days=1, seconds=1)
+        yli_vuorokausi_sitten = timezone.now() - päivä_ja_yksi_sek
+        vanha_kysymys = Kysymys(julkaisupvm=yli_vuorokausi_sitten)
+
+        vastaus = vanha_kysymys.onko_julkaistu_lähiaikoina()
+
+        self.assertIs(vastaus, False)
+
+    def test_onko_julkaistu_lähiaikoina_tuoreella_kysymyksellä(self):
+        """
+        onko_julkaistu_lähiaikoina True jos tuore kysymys
+        """
+        alle_vuorokausi = datetime.timedelta(hours=23, minutes=59, seconds=59)
+        tuore_aika = timezone.now() - alle_vuorokausi
+        tuore_kysymys = Kysymys(julkaisupvm=tuore_aika)
+
+        vastaus = tuore_kysymys.onko_julkaistu_lähiaikoina()
+
+        self.assertIs(vastaus, True)
