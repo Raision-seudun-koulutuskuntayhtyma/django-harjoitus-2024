32ccf14b35ae50bcfa6d5e28c1a2b6a118829ae6
Tuomas Suutari <tuomas@nepnep.net>
2024-02-26 13:49:49 +0200

    Ylikirjoita admin/base_site.html

--- sivusto/settings.py
+++ sivusto/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'sivusto.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / "sivusto" / "templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
